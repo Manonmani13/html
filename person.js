3b11ba753b2cc17fae161d77a7e141415b65c275
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-09-30 21:26:42 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,5 @@
+var pname = "Ashok";
+var age = 26;
+var gender = "male";
+var isEmployee = true;
+console.log("Name =".concat(pname, ", Age = ").concat(age, ", Gender= ").concat(gender, " , isEmployee =").concat(isEmployee));
---
+++ person.ts
@@ -0,0 +1,5 @@
+var pname="Ashok";
+var age:number= 26;
+var gender:string ="male";
+var isEmployee: boolean=true;
+console.log(`Name =${pname}, Age = ${age}, Gender= ${gender} , isEmployee =${isEmployee}`)
